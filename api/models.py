b52a8d45b092c127d1f8a1a587c4e9ad63354e7c
jay3ss <mentoce@gmail.com>
2022-11-07 17:42:24 -0800

    First iteration of separating data processing from classification (broken at  step)

--- api/models.py
+++ api/models.py
@@ -31,81 +31,115 @@ SELECTED_FEATURES = [
 
 
 class Data:
+    def __init__(self, path: str, dropcol=True) -> None:
+        data = pd.read_csv(path)
+        self._data = _preprocess(data, dropcol)
+
+    @property
+    def data(self) -> pd.DataFrame:
+        return self._data
+
+
+class TrainingData(Data):
     def __init__(self, path: str) -> None:
-        self._training_data = pd.read_csv(path)
-        self.features = None
-        self.target = None
-        self.preprocess()
+        super().__init__(path)
+        self._target = self.data["Target"]
+        self._features = self._process_features()
 
-    def preprocess(self) -> 'Data':
-        self._training_data.dropna(axis=0, inplace=True)  # drop rows with null
+    @property
+    def features(self) -> pd.DataFrame:
+        return self._features
 
-        # Extract month, hour, day of week from 'Start_Time' column
-        self._training_data["Start_Time"] = pd.to_datetime(self._training_data["Start_Time"])
-        self._training_data["Start_Month"] = self._training_data["Start_Time"].dt.month
-        self._training_data["Start_Hour"] = self._training_data["Start_Time"].dt.hour
-        self._training_data["Start_Day"] = self._training_data["Start_Time"].dt.dayofweek
-        self._training_data["Start_Day"] = self._training_data["Start_Day"].astype(object)
+    @property
+    def target(self) -> pd.DataFrame:
+        return self._target
 
-        # Drop 'Start_Time' column
-        self._training_data.drop("Start_Time", axis=1, inplace=True)
-
-        # Target labels and features
-        self.target = self._training_data["Target"]
-        x_features = self._training_data.drop("Target", axis=1, inplace=False)
-        x_features_ohe = pd.get_dummies(x_features)
+    def _process_features(self):
+        features = self.data.drop('Target', axis=1, inplace=False)
+        features_ohe = pd.get_dummies(features)
 
         # Features after performing feature selection based on p-values
-        self.features = x_features_ohe[SELECTED_FEATURES]
-        return self
+        return features_ohe[SELECTED_FEATURES]
 
-    def input_data(self, path: str) -> 'Data':
-        input_data = pd.read_csv(path)
-        input_data.dropna(axis=0, inplace=True)  # drop rows with null
 
-        # Extract month, hour, day of week from 'Start_Time' column
-        input_data["Start_Time"] = pd.to_datetime(input_data["Start_Time"])
-        input_data["Start_Month"] = input_data["Start_Time"].dt.month
-        input_data["Start_Hour"] = input_data["Start_Time"].dt.hour
-        input_data["Start_Day"] = input_data["Start_Time"].dt.dayofweek
-        input_data["Start_Day"] = input_data["Start_Day"].astype(object)
+class InputData(Data):
+    def __init__(self, path: str) -> None:
+        super().__init__(path, dropcol=False)
+        columns = ["Start_Time", "Start_Lat", "Start_Lng"]
+        self._index = self._data[columns]
+        self._data_ohe = pd.get_dummies(self.data)[SELECTED_FEATURES]
 
-        # Separately save 'Start_Time', 'Start_Lat', 'Start_Lng' column to output 
-        # with the prediction
-        self.index_df = input_data[["Start_Time", "Start_Lat", "Start_Lng"]]
+    @property
+    def index(self) -> pd.DataFrame:
+        return self._index
 
-        # Drop 'Start_Time' column since you don't need this when modeling
-        input_data.drop("Start_Time", axis=1, inplace=True)
-        self.input_data = input_data
-
-        # one-hot-encoding of categorical features
-        input_features_ohe = pd.get_dummies(self.input_data)
-
-        # Features after performing feature selection based on p-values
-        self.input_features = input_features_ohe[self.selected_features]
-
-        return self
+    @property
+    def data_ohe(self) -> pd.DataFrame:
+        self._data_ohe
 
 
 class Classifier:
-    def __init__(self, path: str, *args, **kwargs) -> None:
+    def __init__(self, tdata: TrainingData, idata: TrainingData, *args, **kwargs) -> None:
         self._classifier = XGBClassifier(*args, **kwargs)
         self._current_prediction = None
-        self._index_df = None
-        self._data = Data(path=path)
+        self._tdata = tdata
+        self._idata = idata
 
     def fit(self) -> 'Classifier':
-        features = self._data.features
-        target = self._data.target
+        features = self._tdata.data
+        target = self._idata.data
+        # TODO: fix the following error:
+        # Exception has occurred: TypeError
+        #   '<' not supported between instances of 'Timestamp' and 'int'
         self._classifier.fit(features, target)
         return self
 
     def predict(self, data: str) -> pd.DataFrame:
         prediction = self._classifier(data)
-        output = self._index_df.copy()
+        output = self._idata.index.copy()
         output["Pred Label"] = prediction
         self._current_prediction = output
         return self._current_prediction
 
     def to_csv(self, path="results.csv"):
         self._current_prediction.to_csv(path, index=False)
+
+
+def _get_time_data(data: pd.DataFrame, dropcol=True) -> list:
+    """
+    Extracts the hour, day, and month of week from the `Start_Time` column
+    formatted as `yyyy-mm-dd HH:MM:SS` and drops the original column and puts
+    the data into `Start_Hour`, `Start_Day`, and `Start_Month` columns
+    respectively. This may drop the `Start_Time` column if `dropcol` is True.
+
+    Args
+    ----
+    data (pd.DataFrame): the DataFrame with the `Start_Time` column to be
+    processed.
+
+    dropcol (bool): drops the `Start_Time` column if True, leaves it in if
+    False.
+
+    Returns
+    -------
+    A new DataFrame with `Start_Hour`, `Start_Day`, and `Start_Month` columns
+    containing hour, day, and month of week, respectively.
+    """
+    data_copy = data.copy()
+    # Extract month, hour, day of week from 'Start_Time' column
+    data_copy["Start_Time"] = pd.to_datetime(data_copy["Start_Time"])
+    data_copy["Start_Month"] = data_copy["Start_Time"].dt.month
+    data_copy["Start_Hour"] = data_copy["Start_Time"].dt.hour
+    data_copy["Start_Day"] = data_copy["Start_Time"].dt.dayofweek
+    data_copy["Start_Day"] = data_copy["Start_Day"].astype(object)
+
+    if dropcol:
+        # Drop 'Start_Time' column since you don't need this when modeling
+        data_copy.drop("Start_Time", axis=1, inplace=True)
+
+    return data_copy
+
+
+def _preprocess(data: pd.DataFrame, dropcol=True) -> pd.DataFrame:
+    data.dropna(axis=0, inplace=True)  # drop rows with null
+    return _get_time_data(data, dropcol=dropcol)
