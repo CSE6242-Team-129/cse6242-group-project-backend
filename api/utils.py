dc74ad211757f777f7e384295c697ffdeeeca803
jay ess <mentoce@gmail.com>
2022-11-09 15:30:41 -0800

    Remove unused code/imports add a lot more...

--- api/utils.py
+++ api/utils.py
@@ -1,98 +1,6 @@
-import ast
-import csv
-import json
 import os
 import sqlite3
-
-import pandas as pd
-
-
-def load_data(filename, headers: bool = True) -> list:
-    """
-    Load the data from the given CSV file
-
-    Params
-    ------
-    filename: the name of the CSV file
-    headers (bool): indicates if the CSV file has headers (True) or not (False)
-
-    Returns
-    -------
-    (list): a list of the data in the CSV file
-    """
-    with open(filename, "r") as f:
-        if headers:
-            reader = csv.DictReader(f)
-        else:
-            reader = csv.reader(f)
-        return [row for row in reader]
-
-
-def extract_lat_lon(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Extracts the latitude and longitude from the 'Location' key the given
-    DataFrame and puts them into their own columns named 'Latitude' and
-    'Longitude', respectively
-
-    """
-
-    def to_tuple(row):
-        d = ast.literal_eval(row)
-        return d["latitude"], d["longitude"]
-
-    # adapted from:
-    # https://gist.github.com/sammatuba/70269c2b5268a83344f5de609ea9b3cc
-    # Line 19
-    df[["latitude", "longitude"]] = pd.DataFrame(
-        df.location.apply(lambda r: to_tuple(r)).tolist(), index=df.index
-    )
-
-    df.latitude = df.latitude.astype(float)
-    df.longitude = df.longitude.astype(float)
-    return df
-
-
-def transform_date_time(df: pd.DataFrame) -> pd.DataFrame:
-    """"""
-    suffix = "T00:00:00.000"
-
-    def get_date(row: str):
-        return row.removesuffix(suffix)
-
-    df["date_occurred"] = pd.DataFrame(
-        df["date_occurred"].apply(lambda r: get_date(r)).tolist(), index=df.index
-    )
-    return df
-
-
-def change_column_names(df: pd.DataFrame) -> pd.DataFrame:
-    """"""
-    df.columns = [
-        old_col_name.replace(" ", "_").replace("(", "").replace(")", "").lower()
-        for old_col_name in df.columns
-    ]
-
-    return df
-
-
-def clean_whitespace(df: pd.DataFrame) -> pd.DataFrame:
-    """"""
-
-    def trim_ws(row):
-        return " ".join(str(row).split()).title()
-
-    df.address = pd.DataFrame(df.address.astype(str).apply(lambda r: trim_ws(r)))
-    df.cross_street = pd.DataFrame(
-        df.cross_street.astype(str).apply(lambda r: trim_ws(r))
-    )
-    return df
-
-
-def load_df(filename, headers: bool = True) -> pd.DataFrame:
-    """"""
-    data = load_data(filename, headers)
-    df = pd.DataFrame(data)
-    return df
+from typing import Union
 
 
 def create_db(db_name: str) -> sqlite3.Connection:
@@ -104,54 +12,91 @@ def create_db(db_name: str) -> sqlite3.Connection:
     return sqlite3.connect(db_name)
 
 
-def connect_to_db(filename: str) -> sqlite3.Connection:
+def connect_to_db(filename: str, debug: bool=True) -> sqlite3.Connection:
     """"""
     conn = sqlite3.connect(filename)
-    conn.row_factory = sqlite3.Row
+    if debug:
+        # easier to debug during development
+        conn.row_factory = dict_factory
+    else:
+        # sqlite3.Row is highly-optimized as a row factory
+        conn.row_factory = sqlite3.Row
     return conn
 
 
-def create_table(conn: sqlite3.Connection) -> sqlite3.Connection:
-    """"""
-    schema = """CREATE TABLE accidents (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        zipcode VARCHAR(5),
-        latitude DOUBLE,
-        longitude DOUBLE,
-        area_name TEXT,
-        area_id INTEGER,
-        street TEXT,
-        cross_street TEXT,
-        time
-    );
-    """
-    conn.execute(schema)
-    return conn
-
-
-def insert_data(conn: sqlite3.Connection, data: list) -> sqlite3.Connection:
-    """"""
-    query = """INSERT INTO accidents (zipcode, latitude, longitude, area_name)
-    VALUES (:zipcode, :latitude, :longitude, :area_name);
+def get_locations_by_zip(conn: sqlite3.Connection, zip_code: Union[str, int]) -> list:
+    query = """
+    SELECT
+        a.hse_nbr,
+        a.hse_frac_nbr,
+        a.hse_dir_cd,
+        a.str_nm,
+        a.str_sfx_cd,
+        a.str_sfx_dir_cd,
+        a.unit_range,
+        a.lat,
+        a.lon,
+        z.zip_code
+    FROM addresses AS a
+    JOIN zip_codes AS z
+    ON z.id = a.zip_code_id
+    WHERE z.zip_code = ?;
     """
     cursor = conn.cursor()
-    cursor.executemany(query, data)
-    conn.commit()
-    cursor.close()
-    return conn
+    result = cursor.execute(query, [zip_code]).fetchall()
+    return result
 
 
-def get_random_entries(conn: sqlite3.Connection, limit: int) -> list:
-    """"""
-    cursor = conn.cursor()
-    results = cursor.execute(
-        f"""SELECT *
-    FROM accidents
-    ORDER BY RANDOM()
-    LIMIT ?;
-    """,
-        [limit],
+
+def dict_factory(cursor: sqlite3.Cursor, row: sqlite3.Row) -> dict:
+    """
+    Converts a sqlite3.Row object to a dictionary. Use this when debugging.
+    Adapted from:
+    https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.row_factory
+
+    Args
+    ----
+    cursor (sqlite3.Cursor): the SQLite database cursor
+    row (sqlite3.Row): the row resulting from the query
+
+    Returns
+    -------
+    (dict): a dictionary of the result
+    """
+    col_names = [col[0] for col in cursor.description]
+    return {key: value for key, value in zip(col_names, row)}
+
+
+def construct_address(d: dict) -> str:
+    """
+    Constructs and address from a row in the 'addresses' table
+
+    Args
+    ----
+    d (dict): the dictionary of the address to be constructed. Example:
+        {
+            'hse_nbr': 388,
+            'hse_frac_nbr': None,
+            'hse_dir_cd': 'W',
+            'str_nm': 'AVENUE 45',
+            'str_sfx_cd': None,
+            'str_sfx_dir_cd': None,
+            'unit_range': None,
+            'lat': 34.09968,
+            'lon': -118.21117,
+            'zip_code': '90065'
+        }
+
+    Returns
+    (str) the constructed address. From the example given above,
+        '388 W Avenue 45'
+    """
+    d_copy = {key: str(val) for key, val in d.items() if not val is None}
+    keys = [
+        'hse_nbr', 'hse_frac_nbr', 'hse_dir_cd', 'str_nm',
+        'str_sfx_cd', 'str_sfx_dir_cd', 'unit_range'
+    ]
+    return ' '.join(
+        val.title() for key, val in d_copy.items()
+        if key in keys
     )
-    # adapted from:
-    # https://nickgeorge.net/programming/python-sqlite3-extract-to-dictionary/
-    return [{k: item[k] for k in item.keys()} for item in results.fetchall()]
