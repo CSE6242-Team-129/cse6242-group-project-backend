b3c78715a9d57bddf5e12c05acfa3688099d85c9
jay3ss <mentoce@gmail.com>
2022-11-22 17:27:42 -0800

    Add OpenWeatherMap API call

--- api/weather.py
+++ api/weather.py
@@ -1,10 +1,16 @@
+from collections import namedtuple
 from functools import lru_cache
+import os
 import warnings
 
+from dotenv import load_dotenv
 from meteostat import Hourly, Stations, units
 import pandas as pd
+import pyowm
 
 
+# load the OpenWeatherMap API key
+load_dotenv()
 # meteostat catches exceptions if a path cannot be loaded and
 # issues a warning instead, leaving us without a way of knowing
 # this without directly checking the returned data. this should
@@ -12,6 +18,19 @@ import pandas as pd
 #
 warnings.filterwarnings("error")
 
+
+owm = pyowm.OWM(os.environ.get("OWM_API_KEY"))
+owm_mgr = owm.weather_manager()
+los_angeles = (
+    owm.city_id_registry()
+        .ids_for("Los Angeles", country="US", matching="exact")
+)[0]
+City = namedtuple('City', ['id', 'name', 'country', 'state', 'lat', 'lon'])
+los_angeles = City(*los_angeles)
+
+
+# TODO: maybe use custom cache maintainer function found here
+# https://stackoverflow.com/a/62843760
 
 # @lru_cache
 def get_hourly_data(location: tuple, period: tuple) -> "pd.DataFrame":
@@ -50,3 +69,33 @@ def transform_data(data: pd.DataFrame) -> pd.DataFrame:
     wdata = data.rename(columns=wc)
     wdata = pd.DataFrame(wdata[list(wc.values())])
     return wdata
+
+
+def get_owm_weather(lat, lon) -> pd.DataFrame:
+    """
+    """
+    # if location:
+    #     lat, lon = location
+    # else:
+    # lat, lon = (location) if location else los_angeles.lat, los_angeles.lon
+    weather = owm_mgr.one_call(lat=lat, lon=lon, units="imperial").current
+    temperature = weather.temp["temp"] # fahrenheit
+    humidity = weather.humidity # in %
+    pressure_conversion = 0.014503773773020924
+    pressure = weather.barometric_pressure("hPa")["press"] * pressure_conversion # in hPa
+    wind_speed = weather.wind("miles_hour")["speed"] # in mph
+    if weather.rain:
+        precipitation = weather.rain # inches
+    else:
+        precipitation = 0
+
+    columns = [
+        'Temperature(F)',
+        'Humidity(%)',
+        'Pressure(in)',
+        'Wind_Speed(mph)',
+        'Precipitation(in)'
+    ]
+    weather_df = pd.DataFrame(columns=columns, index=[0])
+    weather_df[columns] = temperature, humidity, pressure, wind_speed, precipitation
+    return weather_df
