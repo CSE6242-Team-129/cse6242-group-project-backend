a70bbf36eb2903e625f06aca7ba961515d7552e5
jay ess <mentoce@gmail.com>
2022-11-27 18:42:35 -0700

    Add function to get weather by zip code

--- api/weather.py
+++ api/weather.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from meteostat import Hourly, Stations, units
 import pandas as pd
 import pyowm
+from pyowm.utils import measurables as meas
 
 from utils import cache_maintainer
 
@@ -83,6 +84,47 @@ def get_weather_by_lat_lon(lat, lon, type_="pd") -> pd.DataFrame:
 
     weather = owm_mgr.one_call(lat=lat, lon=lon, units="imperial").current
     temperature = weather.temp["temp"] # fahrenheit
+    humidity = weather.humidity # in %
+    pressure_conversion = 0.014503773773020924
+    pressure = weather.barometric_pressure("hPa")["press"] * pressure_conversion # in hPa
+    wind_speed = weather.wind("miles_hour")["speed"] # in mph
+    if weather.rain:
+        precipitation = weather.rain # inches
+    else:
+        precipitation = 0
+
+    if type_ == "pd":
+        columns = [
+            'Temperature(F)',
+            'Humidity(%)',
+            'Pressure(in)',
+            'Wind_Speed(mph)',
+            'Precipitation(in)'
+        ]
+        weather_df = pd.DataFrame(columns=columns, index=[0])
+        weather_df[columns] = temperature, humidity, pressure, wind_speed, precipitation
+        return weather_df
+    elif type_ == "dict":
+        return {
+            'Temperature(F)': temperature,
+            'Humidity(%)': humidity,
+            'Pressure(in)': pressure,
+            'Wind_Speed(mph)': wind_speed,
+            'Precipitation(in)': precipitation
+        }
+
+
+@cache_maintainer(3600) # cache for one hour
+@lru_cache(maxsize=1000)
+def get_weather_by_zip(zip_code: str, type_="pd") -> pd.DataFrame:
+    """
+    """
+    if type_ not in ["dict", "pd"]:
+        raise ValueError(f"type_ must be either 'pd' or 'dict', not '{type_}'")
+
+    weather = owm_mgr.weather_at_zip_code(zip_code, country="US").weather
+    # k_to_fahrenheit = lambda k: 9/5 - 459.67
+    temperature = weather.temperature("fahrenheit")["temp"]
     humidity = weather.humidity # in %
     pressure_conversion = 0.014503773773020924
     pressure = weather.barometric_pressure("hPa")["press"] * pressure_conversion # in hPa
